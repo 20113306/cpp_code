50dd81feddcee13ad2090d48d8045e052048589d
Sungsu <mjk3735@kookmin.ac.kr>
2015-01-21 21:56:26 +0900

    Sungsu

---
+++ DecToHex.py
@@ -0,0 +1,14 @@
+__author__ = 'Sungsu'
+
+def DecToHex():
+	try:
+		num = input("Enter the Decimal Number : ")
+	except:
+		print("Enter the Number not String or Character")
+		DecToHex()
+	else:
+		temp  = str(hex(num))
+		result = temp.split('x')
+		print(result[1])
+
+DecToHex() 
